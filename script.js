2418f2d19c07103eb229aa4032f95e9e8fbeabac
imad273 <abbadimad0123@gmail.com>
2024-07-21 15:25:40 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Animation scroll initialize */
+AOS.init({
+  duration: 1200,
+});
+
+function openPhoneMenu() {
+  var x = document.getElementById("links");
+  x.style.display = "flex";
+  x.style.position = "fixed";
+}
+
+function closeMenu() {
+  var x = document.getElementById("links");
+  x.style.display = "none";
+  x.style.position = "static";
+}
